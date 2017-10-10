39d7f71a8f876797245fea61c9b3023250f47a44
David Reep <davereep@gmail.com>
2017-10-10 21:31:29 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(num)
   if num % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif num % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif num % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   end
 end
